ae552b2afbda6257fc51498a53e9e358dacbc9db
Moses Samuel <mosamuel98@gmail.com>
2019-10-15 15:39:44 -0700

    configure sessions that persist to the db and use the restricted router

--- api/server.js
+++ api/server.js
@@ -1,9 +1,35 @@
+// npm packages
 const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
+const sessions = require('express-session')
+const KnexSessionStore = require('connect-session-knex')(sessions)
+
+// Routers
 const rootRouter = require('./routes/rootRouter')
 const usersRouter = require('./routes/usersRouter')
 const restrictedRouter = require('./routes/restrictedRouter')
+
+// Custom middleware
+const requireActiveSession = require('./middleware/requireActiveSession')
+
+
+const sessionConfiguration = {
+  name: 'webauth-i-challenge',
+  secret: process.env.SESSION_SECRET || 'this is not secret',
+  cookie: {
+    httpOnly: process.env.NODE_ENV === 'production' ? true : false,
+    maxAge: 1000 * 60 * 60, // sessions last for one hour
+    secure: process.env.NODE_ENV === 'production' ? true : false,
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new KnexSessionStore({
+    knex: require('./data/dbClient'),
+    createtable: true,
+    clearInterval: 1000 * 60 * 30 // delete expired sessions every 30 min
+  })
+}
 
 /**
  * Constants 
@@ -23,11 +49,15 @@ const server = express()
  * Middleware
  */
 
-server.use(helmet(), cors(), express.json())
+server.use(sessions(sessionConfiguration))
+server.use(helmet())
+server.use(cors())
+server.use(express.json())
+
 server.use('/', rootRouter)
 server.use('/api', rootRouter)
 server.use('/api/users', usersRouter)
-// server.use('/api/restricted', restrictedRouter)
+server.use('/api/restricted', requireActiveSession, restrictedRouter)
 
 /**
  * Error Handlers
@@ -54,7 +84,7 @@ server.use((err, req, res, next) => {
  * Start server
  */
 
-server.listen(PORT, () => 'server running on port ' + PORT)
+server.listen(PORT, () => console.log('server running on port ' + PORT))
 
 /**
  * Export app
