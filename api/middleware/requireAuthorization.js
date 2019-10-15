cf76eefd8b924b36d717e326ce2bdc47cb715e0a
Moses Samuel <mosamuel98@gmail.com>
2019-10-14 17:03:35 -0700

    modify how error is forwarded in auth middleware

--- api/middleware/requireAuthorization.js
+++ api/middleware/requireAuthorization.js
@@ -2,7 +2,10 @@ function requireAuthorization(req, res, next) {
     if (req.body && req.body.username && req.body.password) {
         next()
     } else {
-        res.status(401).json({ error: { message: "You are missing a username and/or password in your request body." } })
+        const err = new Error("You are missing a username and/or password in your request body.")
+        err.status = 401
+        
+        next(err)
     }
 }
 
