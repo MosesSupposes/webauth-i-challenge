3bd73176f11aea362a1b878a1ef5560ff57b5eb2
Moses Samuel <mosamuel98@gmail.com>
2019-10-14 17:02:42 -0700

    add root and user routers

---
+++ api/routes/usersRouter.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const UsersController = require('../controllers/UsersController')
+const requireAuthorization = require('../middleware/requireAuthorization')
+
+const router = express.Router()
+
+router.get('/', UsersController.index)
+router.post('/register', requireAuthorization, UsersController.register)
+router.post('/login', requireAuthorization,  UsersController.login)
+
+module.exports = router
