8a44ebe3ac713c5a5f0ce172efb6a7bbe1f001b9
Moses Samuel <mosamuel98@gmail.com>
2019-10-14 18:36:11 -0700

    make username a unique field in the users schema

--- api/data/migrations/20191014142315_users.js
+++ api/data/migrations/20191014142315_users.js
@@ -3,7 +3,7 @@ exports.up = function(knex) {
   return knex.schema.createTable('users', tbl => {
       tbl.increments()
       tbl.timestamps(true, true)
-      tbl.string('username', 20).notNullable()
+      tbl.string('username', 20).notNullable().unique()
       tbl.string('password').notNullable()
   })
 };
