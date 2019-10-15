f8f48ac341910e5d7d8e0f39e0c256038387e634
Moses Samuel <mosamuel98@gmail.com>
2019-10-15 15:40:42 -0700

    create session when user logs in and add new controller/endpoint for logging out

--- api/controllers/UsersController.js
+++ api/controllers/UsersController.js
@@ -3,19 +3,21 @@ const UserModel = require('../models/User')
 
 class UserController {
     static async index(req, res) {
-        const users = await UserModel.all()
-        if (!Object.keys(users).length) res.status(404).json({error: { message: "There are no users in the database yet." } })
-        else res.status(200).json(users)
+        try {
+            const users = await UserModel.all()
+            res.status(200).json(users)
+        } catch(e) {
+            res.status(404).json({error: { message: "There are no users in the database yet." } })
+        }
     }
 
     static async register(req, res) {
         try {
             const { username, password } = req.body
-            const hashedPassword = bcrypt.hashSync(password, 8)
             
             const [newUserCreds] = await UserModel.create({
                 username,
-                password: hashedPassword
+                password: bcrypt.hashSync(password, 8)
             })
             
             res.status(201).json(newUserCreds)
@@ -32,6 +34,8 @@ class UserController {
             const user = await UserModel.findByUsername(req.body.username)
 
             if (user && bcrypt.compareSync(req.body.password, user.password)) {
+                req.session.username = user.username
+                
                 res.status(200).json({
                     success: "Welcome " + user.username,
                     user
@@ -43,6 +47,12 @@ class UserController {
             console.log(e)
             res.status(500).json({error: { message: "A user with the username of " + req.body.username + " does not exist."}})
         }
+    }
+
+    static async logout(req, res) {
+        req.session.destroy(err => {
+            res.status(200).json({message: "Goodbye! 👋🏾"})
+        })
     }
 }
 
